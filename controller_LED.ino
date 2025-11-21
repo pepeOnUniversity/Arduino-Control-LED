b61ba4a0e24d1aba5de6659d8a77f52c82369595
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-11-21 13:45:18 +0700

    use PULL_UP for button

--- controller_LED.ino
+++ controller_LED.ino
@@ -5,13 +5,15 @@ int button = 2;
 
 void setup() {
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
+  pinMode(button, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop() {
   //read value of button
   int state = digitalRead(button);
 
+  Serial.println(state);
   //check value of state
   if(state == LOW){
     digitalWrite(led, LOW);
